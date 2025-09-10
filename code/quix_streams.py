6198f551672cf41a3ed8e3a2e79730b07a220b6e
AM <>
2025-09-10 10:48:36 +0300

    Initial commit

---
+++ code/quix_streams.py
@@ -0,0 +1,23 @@
+import os
+import re
+from quixstreams import Application
+
+app = Application(
+    broker_address="localhost:9092",
+    consumer_group="word_counter",
+    auto_offset_reset="earliest"
+)
+
+words_topic = app.topic(name="test_topic")
+word_length_topic = app.topic(name="word_length_topic")
+
+sdf = app.dataframe(topic=words_topic)
+
+sdf = sdf.apply(lambda text: len(text))
+sdf = sdf.hopping_window(duration_ms=5000, step_ms=1000).mean().current()
+
+sdf = sdf.to_topic(word_length_topic)
+
+if __name__ == "__main__":
+    # Start message processing
+    app.run(sdf)
